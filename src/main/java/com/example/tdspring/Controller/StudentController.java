a1d148e90916ba0b3c003841fcd3466528d6b12c
Fiderana-17 <hei.fiderana@gmail.com>
2026-03-18 13:47:21 +0300

    feat: implement TD2 => request handling with body

--- src/main/java/com/example/tdspring/Controller/StudentController.java
+++ src/main/java/com/example/tdspring/Controller/StudentController.java
@@ -1,14 +1,32 @@
 package com.example.tdspring.Controller;
 
+import com.example.tdspring.Entity.Student;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
     @RestController
     public class StudentController {
+
+        private List<Student> students = new ArrayList<>();
 
         @GetMapping("/welcome")
         public String welcome(@RequestParam String name) {
             return "Welcome " + name;
         }
 
+        @PostMapping("/students")
+        public String addStudents(@RequestBody List<Student> newStudents) {
+
+            students.addAll(newStudents);
+
+            String result = "";
+            for (Student s : students) {
+                result += s.getFirstName() + " " + s.getLastName() + "\n";
+            }
+
+            return result;
+        }
     }
 
