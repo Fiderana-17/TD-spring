73bfc15fd91b15aa4f7b26efedadeb520b50cb37
Fiderana-17 <hei.fiderana@gmail.com>
2026-03-23 09:09:15 +0300

    feat(TD3): implement post mapping

--- src/main/java/com/example/tdspring/Controller/StudentController.java
+++ src/main/java/com/example/tdspring/Controller/StudentController.java
@@ -1,10 +1,13 @@
 package com.example.tdspring.Controller;
 
+import com.example.tdspring.Entity.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-    @RestController
+import java.util.List;
+
+@RestController
     public class StudentController {
 
         @GetMapping("/welcome")
@@ -19,6 +22,25 @@ import org.springframework.web.bind.annotation.*;
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body("Welcome " + name);
+        }
+
+        // B) POST /students
+        @PostMapping("/students")
+        public ResponseEntity<?> addStudents(@RequestBody List<Student> newStudents) {
+
+            try {
+                students.addAll(newStudents);
+
+                return ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body(students);
+
+            } catch (Exception e) {
+
+                return ResponseEntity
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Server error");
+            }
         }
     }
 
